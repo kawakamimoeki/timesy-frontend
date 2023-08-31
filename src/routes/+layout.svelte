5a31cdd52554b3daef7fb636697bf43ba694c098
Moeki Kawakami <me@moeki.dev>
2023-08-31 22:57:03 +0900

    やっぱり同期的に読み込んだほうが良いかも

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import "../app.css";
-  import "../css/activities.css"
-  import "../css/codemirror.css";
-  import "../css/global.css";
-  import "../css/markdown.css";
-  import "../css/modal.css";
+	import '../css/app.css';
+	import '../css/activities.css';
+	import '../css/codemirror.css';
+	import '../css/global.css';
+	import '../css/markdown.css';
+	import '../css/modal.css';
 </script>
 
 <slot />
--- src/routes/trending/+page.server.ts
+++ src/routes/trending/+page.server.ts
@@ -1,7 +1,7 @@
 import { BACKEND } from '$env/static/private';
 
 export const load = async () => {
-	const res = await fetch(`${BACKEND}/api/v1/posts`);
+	const res = await fetch(`${BACKEND}/api/v1/posts/trending`);
 	const json = await res.json();
 	return {
 		posts: json.posts
